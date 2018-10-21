6c93535e4c4f600323a2a7c14321e18297c112da
victorphellyppe <victorphellyppe@hotmail.com>
2018-10-20 21:11:20 -0300

    Retirando os radicais das palavras com stremming

---
+++ radicaisDasPalavras.py
@@ -0,0 +1,54 @@
+import nltk
+#nltk.download()
+
+base = [('eu sou admirada por muitos','alegria'),
+        ('me sinto completamente amado','alegria'),
+        ('amar e maravilhoso','alegria'),
+        ('estou me sentindo muito animado novamente','alegria'),
+        ('eu estou muito bem hoje','alegria'),
+        ('que belo dia para dirigir um carro novo','alegria'),
+        ('o dia está muito bonito','alegria'),
+        ('estou contente com o resultado do teste que fiz no dia de ontem','alegria'),
+        ('o amor e lindo','alegria'),
+        ('nossa amizade e amor vai durar para sempre', 'alegria'),
+        ('estou amedrontado', 'medo'),
+        ('ele esta me ameacando a dias', 'medo'),
+        ('isso me deixa apavorada', 'medo'),
+        ('este lugar e apavorante', 'medo'),
+        ('se perdermos outro jogo seremos eliminados e isso me deixa com pavor', 'medo'),
+        ('tome cuidado com o lobisomem', 'medo'),
+        ('se eles descobrirem estamos encrencados', 'medo'),
+        ('estou tremendo de medo', 'medo'),
+        ('eu tenho muito medo dele', 'medo'),
+        ('estou com medo do resultado dos meus testes', 'medo')]
+#print(base[6])
+
+
+stopwords = ['a', 'agora', 'algum', 'alguma', 'aquele', 'aqueles', 'de', 'deu', 'do', 'e', 'estou', 'esta', 'esta',
+             'ir', 'meu', 'muito', 'mesmo', 'no', 'nossa', 'o', 'outro', 'para', 'que', 'sem', 'talvez', 'tem', 'tendo',
+             'tenha', 'teve', 'tive', 'todo', 'um', 'uma', 'umas', 'uns', 'vou']
+#palavras que não dão siginificado a nosso modelo
+stopwordsnltk = nltk.corpus.stopwords.words("portuguese")
+
+def removeStopWords(texto):
+    frases = []
+    for (palavras, emocao) in texto:
+        semstop = [p for p in palavras.split() if p not in stopwordsnltk]
+        frases.append((semstop, emocao))
+    return frases
+print(removeStopWords(base))
+
+def aplicastemmer(texto):
+    stemmer = nltk.stem.RSLPStemmer()
+    frasestmming = []
+    for (palavras, emocao) in texto:
+        comstemming = [str(stemmer.stem(p)) for p in palavras.split() if p not in stopwordsnltk]
+        frasestmming.append((comstemming, emocao))
+    return frasestmming
+
+frasescomstemming = aplicastemmer(base)
+print(frasescomstemming)
+
+
+
+
