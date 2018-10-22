bf4d67af18cf182806315bc1e87bc2de69001092
victorphellyppe <victorphellyppe@hotmail.com>
2018-10-21 22:26:11 -0300

    Unique words function

---
+++ palavrasUnicas.py
@@ -0,0 +1,7 @@
+##funcao busca palavras unicas
+def buscaPalavrasUnicas(frequencia):
+    freq = frequencia.keys()
+    return freq
+
+palavrasunicas = buscaPalavrasUnicas(frequencia)
+print(palavrasunicas)
